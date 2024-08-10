7320b2aeb64e020183da027c28de4be95c108dfb
Xing Ji <ji.xing@tarsocial.com>
2024-08-10 14:17:50 +0800

    init my nvim config

---
+++ default_config/_machine_specific_default.lua
@@ -0,0 +1,16 @@
+-- please read through this file and change what you need to change
+
+vim.g.snips_author = 'dzmfg'
+
+vim.g.python3_host_prog = '/opt/homebrew/Caskroom/miniforge/base/bin/python3'
+
+-- vim.g.mkdp_browser = 'safari'
+-- vim.g.mkdp_browserfunc = 'open '
+
+-- vim.g.mkdp_browserfunc = 'open '
+
+-- vim.g.flutter_default_device = 'macos'
+-- vim.g.flutter_run_args = ''
+-- vim.g.flutter_format_line_length = 100
+
+-- vim.g.codelldb_path = '/Users/david/prog/codellvm/extension/adapter/codelldb'
