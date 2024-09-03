7ab99f151e5a8ef0462f975baf1af96a6319b737
Ji Xing <dzmfg@Jis-MacBook-Pro.local>
2024-09-03 16:54:02 +0800

    add filemanager.lua

---
+++ lua/config/plugins/filemanager.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	},
+	{
+		"micsama/joshuto.nvim",
+		lazy = true,
+		cmd = "Joshuto",
+		config = function()
+			vim.g.joshuto_floating_window_scaling_factor = 0.85
+			vim.g.joshuto_use_neovim_remote = 1
+			vim.g.joshuto_floating_window_winblend = 0
+		end
+	}
+}
