76fea94b2eeb8703b2cbbeb83fdacfc4bb1682e2
Ji Xing <dzmfg@Jis-MacBook-Pro.local>
2024-09-08 00:13:09 +0800

    change theme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
-	"micsama/nvim-deus",
-	lazy = false,
-	priority = 1000,
+	"olimorris/onedarkpro.nvim",
+	priority = 1000, -- Ensure it loads first
 	config = function()
-		vim.cmd([[colorscheme deus]])
-	end,
+		vim.cmd("colorscheme onedark")
+	end
 }
+-- return {
+-- 	"micsama/nvim-deus",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd([[colorscheme deus]])
+-- 	end,
+-- }
+
