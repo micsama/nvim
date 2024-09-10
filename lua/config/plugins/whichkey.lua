8ca9f6bcf14901f6d1c58a502f4c5709c10d3161
Ji Xing <dzmfg@Jis-MacBook-Pro.local>
2024-09-11 00:09:15 +0800

    add which key

---
+++ lua/config/plugins/whichkey.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+		config = function()
+			require("which-key").add({
+				{ "<leader>ff", group = "FindFile", icon = "a" },
+			})
+		end
+	},
+	-- {
+	-- 	'mrjones2014/legendary.nvim',
+	-- 	-- since legendary.nvim handles all your keymaps/commands,
+	-- 	-- its recommended to load legendary.nvim before other plugins
+	-- 	priority = 10000,
+	-- 	lazy = false,
+	-- 	-- sqlite is only needed if you want to use frecency sorting
+	-- 	-- dependencies = { 'kkharji/sqlite.lua' }
+	-- }
+}
