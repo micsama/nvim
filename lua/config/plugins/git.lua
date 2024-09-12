5ba9425f269d6e8487ee95933a2b393f38690495
Ji Xing <dzmfg@Jis-MacBook-Pro.local>
2024-09-12 21:24:33 +0800

    修改字体等

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		event = { 'BufReadPre', 'BufNewFile' }, -- 当打开文件时加载
+		event = { 'BufReadPre', 'BufNewFile' },
 		config = function()
 			require('gitsigns').setup({
 				signs = {
@@ -13,28 +13,24 @@ return {
 					untracked    = { text = '┆' },
 				},
 			})
-			vim.keymap.set("n", "<leader>g-", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>g=", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "H", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
-		end
+		end,
+		keys = {
+			{ "<leader>g-", ":Gitsigns prev_hunk<CR>",    { noremap = true, silent = true } },
+			{ "<leader>g=", ":Gitsigns next_hunk<CR>",    { noremap = true, silent = true } },
+			{ "<leader>gb", ":Gitsigns blame_line<CR>",   { noremap = true, silent = true } },
+			{ "<leader>gr", ":Gitsigns reset_hunk<CR>",   { noremap = true, silent = true } },
+			{ "H",          ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true } },
+		},
 	},
 	{
 		"kdheepak/lazygit.nvim",
-		keys = { "<a-g>" },
+		keys = {
+			{ "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true } },
+		},
 		config = function()
 			vim.g.lazygit_floating_window_scaling_factor = 1.0
 			vim.g.lazygit_floating_window_winblend = 0
 			vim.g.lazygit_use_neovim_remote = true
-			vim.keymap.set("n", "<a-g>", ":LazyGit<CR>", { noremap = true, silent = true })
-		end
+		end,
 	},
-	-- {
-	-- 	"APZelos/blamer.nvim",
-	-- 	config = function()
-	-- 		vim.g.blamer_enabled = true
-	-- 		vim.g.blamer_relative_time = true
-	-- 	end
-	-- }
 }
