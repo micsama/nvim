9cc294a33ca631ae28cd7ffb5d573201b00f0b99
Ji Xing <dzmfg@Jis-MacBook-Pro.local>
2024-09-24 00:57:58 +0800

    add markdown preview

--- lua/config/plugins/language/markdown.lua
+++ lua/config/plugins/language/markdown.lua
@@ -1,19 +1,29 @@
 return {
-	-- {
-	-- 	"instant-markdown/vim-instant-markdown",
-	-- 	ft = { "markdown" },
-	-- 	build = "yarn install",
-	-- 	config = function()
-	-- 		vim.g.instant_markdown_autostart = 0
-	-- 	end,
-	-- },
 	{
 		"OXY2DEV/markview.nvim",
-		lazy = false, -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
+		-- lazy = false, -- Recommended
+		ft = "markdown", -- If you decide to lazy-load anyway
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons"
+		},
+		keys = {
+			{ "<leader>mk", "<CMD>Markview<CR>", desc = "Toggle Markdown View" }
+		},
+		opts = {
+			modes = { "n", "i", "no", "c" },
+			hybrid_modes = { "i" },
+			callbacks = {
+				on_enable = function(_, win)
+					vim.wo[win].conceallevel = 2;
+					vim.wo[win].concealcursor = "nc";
+				end
+			}
 		}
+	},
+	{
+		'Kicamon/markdown-table-mode.nvim',
+		ft = "markdown", -- If you decide to lazy-load anyway
+		opts = {},
 	}
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,2 @@
 # nvim config for dzmfg
 
-## TODO：
-
-1. markdown预览等事项
-2. 快捷键记录。
-3. asdasd
