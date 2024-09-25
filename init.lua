5105c5854248844a0d0a89d241bec971dd02cd38
micsama <1246659083@qq.com>
2024-09-26 00:34:18 +0800

    linux设置分开

--- init.lua
+++ init.lua
@@ -1,6 +1,18 @@
 require("config.defaults")
-require("config.plugins")
+-- 获取系统信息
+local uname = vim.uv.os_uname()
+
+-- 检查系统类型
+if uname.sysname == "Linux" then
+	vim.o.shell = "/usr/bin/bash"
+elseif uname.sysname == "Darwin" then
+	vim.o.shell = "/opt/homebrew/bin/fish"
+else
+	vim.o.shell = "/usr/bin/bash"
+end
+
 require("config.keymaps")
+require("config.plugins")
 if vim.g.neovide then
 	require("config.neovide")
 end
