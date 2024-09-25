41174746bc4cd2d32e28dda986c532adfa11e210
micsama <1246659083@qq.com>
2024-09-26 00:34:06 +0800

    修复project等

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -3,7 +3,15 @@ return {
 		"folke/todo-comments.nvim",
 		-- event = "BufRead", -- Lazy load when a buffer is read
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {}
+		opts = {
+			keywords = {
+				MODIFIED = {
+					icon = " ",
+					color = "hint",
+					alt = { "CHANGED", "UPDATED", "MOD" }
+				},
+			}
+		}
 	},
 	{
 		'numToStr/Comment.nvim',
