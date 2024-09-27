50b1d0a21bc4038d45d2c111ade3e6a4cd024fe1
micsama <1246659083@qq.com>
2024-09-27 13:52:00 +0800

    joshto->yazi; fix bigfile; sj, sk, sh, sl switch map

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
-require("config.defaults")
-require("config.keymaps")
-require("config.plugins")
+require("config.defaults") -- 基础设置配置
+require("config.keymaps")  -- 设置所有与插件无关的快捷键
+require("config.plugins")  -- 设置插件以及插件相关快捷键
--- lua/config/plugins/filemanager.lua
+++ lua/config/plugins/filemanager.lua
@@ -65,14 +65,34 @@ return {
 			})
 		end
 	},
+	---@type LazySpec
 	{
-		"micsama/joshuto.nvim",
-		lazy = true,
-		cmd = "Joshuto",
-		config = function()
-			vim.g.joshuto_floating_window_scaling_factor = 0.85
-			vim.g.joshuto_use_neovim_remote = 1
-			vim.g.joshuto_floating_window_winblend = 0
-		end
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<s-r>",
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				'<c-up>',
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		},
+		---@type YaziConfig
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = false,
+			keymaps = {
+				show_help = '<f1>',
+			},
+		},
 	}
 }
