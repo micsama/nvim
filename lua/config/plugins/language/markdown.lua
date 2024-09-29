3ba42958eabbe485cd90636e235fbf14db571e44
micsama <1246659083@qq.com>
2024-09-29 21:02:13 +0800

    add neo-tree

--- lua/config/plugins/language/markdown.lua
+++ lua/config/plugins/language/markdown.lua
@@ -3,7 +3,7 @@ return {
 		"OXY2DEV/markview.nvim",
 		-- lazy = false, -- Recommended
 		ft = "markdown", -- If you decide to lazy-load anyway
-		branch = "dev",
+		-- branch = "dev",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons"
