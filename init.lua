c07dc4e289b02e452d02855a6aac333fd4284584
micsama <1246659083@qq.com>
2025-09-29 19:20:42 +0800

    调整git的位置

--- init.lua
+++ init.lua
@@ -2,10 +2,27 @@ vim.g.mapleader = " "       -- 将<Leader>键设置为空格键
 vim.g.maplocalleader = "\\" -- 将本地<Leader>键设置为反斜杠
 require("config.defaults")  -- 设置默认的一些配置
 require("config.keymaps")   -- 设置快捷键
-if vim.g.neovide then
-	require("config.neovide")
+-- if vim.g.neovide then
+-- 	require("config.neovide")
+-- end
+-- require("dzmfg") -- 基础mini设置配置
+
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		'git', 'clone', '--filter=blob:none',
+		'https://github.com/nvim-mini/mini.nvim', mini_path
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd('packadd mini.nvim | helptags ALL')
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
-require("dzmfg") -- 基础mini设置配置
+    vim.pack.add{"https://github.com/kdheepak/lazygit.nvim"}
+    vim.g.lazygit_floating_window_scaling_factor = 1.0
+    vim.g.lazygit_floating_window_winblend = 0
+    vim.g.lazygit_use_neovim_remote = true
 
 local uname = vim.uv.os_uname()
 -- -- 检查系统类型
