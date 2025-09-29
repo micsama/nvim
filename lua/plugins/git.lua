98414379e374bae55174d008b4bce23697ed02ac
micsama <1246659083@qq.com>
2025-09-29 23:56:46 +0800

    修改细节

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,21 +2,25 @@ local now, add, later = MiniDeps.now, MiniDeps.add, MiniDeps.later
 local map = require("util.utils").map
 
 later(function()
-    add("lewis6991/gitsigns.nvim")
-    require('gitsigns').setup({
-        signs = {
-            add          = { text = '▎' },
-            change       = { text = '░' },
-            delete       = { text = '_' },
-            topdelete    = { text = '▔' },
-            changedelete = { text = '▒' },
-            untracked    = { text = '┆' },
-        },
-    })
+	add("lewis6991/gitsigns.nvim")
+	require('gitsigns').setup({
+		signs = {
+			add          = { text = '▎' },
+			change       = { text = '░' },
+			delete       = { text = '_' },
+			topdelete    = { text = '▔' },
+			changedelete = { text = '▒' },
+			untracked    = { text = '┆' },
+		},
+	})
 
-    map("nv", "<leader>g-", ":Gitsigns prev_hunk<CR>", "prev_hunk")
-    map("nv", "<leader>g=", ":Gitsigns next_hunk<CR>", "next_hunk")
-    map("nv", "<leader>gb", ":Gitsigns blame_line<CR>", "blame_line")
-    map("nv", "<leader>gr", ":Gitsigns reset_hunk<CR>", "reset_hunk")
-    map("nv", "H", ":Gitsigns preview_hunk<CR>", "preview_hunk")
+	map("nv", "<leader>g-", ":Gitsigns prev_hunk<CR>", "prev_hunk")
+	map("nv", "<leader>g=", ":Gitsigns next_hunk<CR>", "next_hunk")
+	map("nv", "<leader>gb", ":Gitsigns blame_line<CR>", "blame_line")
+	map("nv", "<leader>gr", ":Gitsigns reset_hunk<CR>", "reset_hunk")
+	map("nv", "H", ":Gitsigns preview_hunk<CR>", "preview_hunk")
 end)
+
+vim.g.lazygit_floating_window_scaling_factor = 1.0
+vim.g.lazygit_floating_window_winblend = 0
+vim.g.lazygit_use_neovim_remote = true
