093af0f7732bae751c360acc123d4347b2eb870f
micsama <1246659083@qq.com>
2025-10-07 21:38:06 +0800

    更新了字符串的单双引号

--- lua/config/dzmfg.lua
+++ lua/config/dzmfg.lua
@@ -1,8 +1,6 @@
--- 主题和核心插件初始化 (Theming and Core Plugin Setup)
-require("plugins.mini") -- 配置 mini家族的 核心插件
-require("onedarkpro").setup({
+require('onedarkpro').setup({
 	colors = {
-		cursorline = "#303442" -- 自定义光标行颜色 (可选，默认基于背景)
+		cursorline = '#303442' -- 自定义光标行颜色 (可选，默认基于背景)
 	},
 	options = {
 		cursorline = true -- 启用 onedarkpro 对光标行的着色
@@ -10,13 +8,16 @@ require("onedarkpro").setup({
 })
 vim.cmd.colorscheme('onedark') -- 应用 onedark 主题
 
+-- 主题和核心插件初始化 (Theming and Core Plugin Setup)
+require('plugins.mini') -- 配置 mini家族的 核心插件
+
 -- 加载功能模块配置 (Loading Functional Plugin Configurations)
-require("plugins.Ui")          -- 用户界面和外观
-require("plugins.lsp")         -- 语言服务器协议 (LSP)
-require("plugins.editor")      -- 编辑器增强功能
-require("plugins.filemanager") -- 文件管理/导航
-require("plugins.dap")         -- 调试适配器协议 (DAP)
-require("plugins.cmp")         -- 补全引擎 (Completion)
-require("plugins.other")       -- 其他通用插件
-require("plugins.llm")         -- 大型语言模型相关插件
-require("plugins.git")         -- Git 集成插件
+require('plugins.Ui')          -- 用户界面和外观
+require('plugins.lsp')         -- 语言服务器协议 (LSP)
+require('plugins.editor')      -- 编辑器增强功能
+require('plugins.filemanager') -- 文件管理/导航
+require('plugins.dap')         -- 调试适配器协议 (DAP)
+require('plugins.cmp')         -- 补全引擎 (Completion)
+require('plugins.other')       -- 其他通用插件
+require('plugins.llm')         -- 大型语言模型相关插件
+require('plugins.git')         -- Git 集成插件
