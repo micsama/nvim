64f96acb834e0ae1427f16f314ce30c7ae94a42c
micsama <1246659083@qq.com>
2026-01-07 13:44:57 +0800

    refactor(plugins): 优化 Telescope 快捷键映射与功能布局
    - 将快捷键映射统一移至 `util.utils.map` 函数调用
    - 新增 `<leader>fr` 用于最近打开文件，`<leader>fw` 用于搜索光标下单词
    - 新增 `<leader>fs` 用于语法树符号，`<leader>fc` 用于搜索历史
    - 移除原有的 `<leader>fb` 和 `<leader>fh` 映射
    - 为映射描述添加图标，提升可读性

--- lua/plugins/Ui.lua
+++ lua/plugins/Ui.lua
@@ -93,7 +93,15 @@ local builtin = require("telescope.builtin")
 require("telescope").load_extension("fzf")
 
 -- Telescope 快捷键映射
-map("nv", "<leader>ff", builtin.find_files, "Find Files")
-map("nv", "<leader>fg", builtin.live_grep, "Live Grep")
-map('nv', '<leader>fb', builtin.buffers, 'Find Buffers')
-map('nv', '<leader>fh', builtin.help_tags, 'Find Help Tags')
+local map = require("util.utils").map
+local builtin = require('telescope.builtin')
+
+-- 文件与历史查找
+map("nv", "<leader>ff", builtin.find_files, "🔍 查找文件")
+map("nv", "<leader>fr", builtin.oldfiles, "🕒 最近打开") -- 改为 fr (Recent)
+map("nv", "<leader>fg", builtin.live_grep, "🛰️ 全局搜索")
+map("nv", "<leader>fw", builtin.grep_string, "词 关键词搜索") -- 搜索光标下单词
+
+-- 状态与导航
+map("nv", "<leader>fs", builtin.treesitter, "🌳 语法树符号") -- 改为 fs (Symbol)
+map("nv", "<leader>fc", builtin.search_history, "📜 搜索历史") -- 改为 fc (Command history)
